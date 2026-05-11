921af56f0ae67d190c78d1fe2a6a0142093dcfa9
Aravind S <227003016@sastra.ac.in>
2026-05-11 11:42:52 +0530

    add render.yaml and build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 pip install --upgrade pip
-pip install --only-binary=:all: scikit-learn==1.3.2
-pip install --only-binary=:all: pandas==2.0.3
-pip install --only-binary=:all: numpy==1.24.4
+pip install pandas==2.0.3 --only-binary=:all:
+pip install numpy==1.24.4 --only-binary=:all:
+pip install scikit-learn==1.3.2 --only-binary=:all:
 pip install flask==3.0.3
 pip install flask-cors==4.0.1
 pip install joblib==1.3.2
 pip install gunicorn==21.2.0
+pip list
 python train_model.py
